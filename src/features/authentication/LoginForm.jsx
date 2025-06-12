53e48ed1338de02ef0896a95564640413d40ba23
Mathias Heartman <161854301+Mathiasheartman@users.noreply.github.com>
2025-06-12 20:48:46 +0100

    Update LoginForm.jsx

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("testuser@gkjh.com");
+  const [password, setPassword] = useState("1234567890");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
